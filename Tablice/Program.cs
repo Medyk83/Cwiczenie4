512efe6a1c2e8cf078191bcfd9523615c7bcd0e1
Adrian Medynski <128226981+Medyk83@users.noreply.github.com>
2023-09-01 08:20:31 +0200

    Opis

---
+++ Tablice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] tablica = { 7, 3, 9, 4, 9, 2 };
+
+        int najmniejszy = tablica[0];
+        int najwiekszy = tablica[0];
+
+        for (int i = 1; i < tablica.Length; i++)
+        {
+            if (tablica[i] < najmniejszy)
+                najmniejszy = tablica[i];
+
+            if (tablica[i] > najwiekszy)
+                najwiekszy = tablica[i];
+        }
+
+        Console.WriteLine("Najmniejszy element: " + najmniejszy);
+        Console.WriteLine("Największy element: " + najwiekszy);
+        Console.ReadLine();
+    }
+}
